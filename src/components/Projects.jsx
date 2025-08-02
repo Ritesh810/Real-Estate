3c5d765617c3b715853924f87cfe9bc1b2578292
Ritesh Narola <riteshnarola810@gmail.com>
2025-08-02 11:24:29 +0530

    immproved animaiton

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ function Projects() {
 
   return (
     <motion.div 
-    initial={{ opacity: 0, x: 200 }}
+    initial={{ opacity: 0, x: -200 }}
     transition={{ duration: 1 }}
     whileInView={{ opacity: 1, x: 0 }}
     viewport={{ once: true }}
